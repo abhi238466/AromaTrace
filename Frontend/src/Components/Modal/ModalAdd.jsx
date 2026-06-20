78b1c3ad68a10e2b9a882fab4bf558ae8603d209
Abhishek Kumar <abhikumar845422@gmail.com>
2026-06-20 19:49:29 +0530

    week 3 updates

--- Frontend/src/Components/Modal/ModalAdd.jsx
+++ Frontend/src/Components/Modal/ModalAdd.jsx
@@ -3,7 +3,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useForm } from 'react-hook-form';
 import toast, { Toaster } from 'react-hot-toast';
 import baseUrl from '../../utils/baseurl';
-// import { setProducts } from '../../Redux/products/productSlice';
+import { setProducts } from '../../Redux/products/productSlice';
+
 
 const ModalAdd = (props) => {
     const products = useSelector((state) => state.product.products);
