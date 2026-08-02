8a6a21edabf9109ff342d3d6216c02ef6e5c9bd6
Abhishek Kumar <abhikumar845422@gmail.com>
2026-08-02 14:26:28 +0530

    feat: configure peoduction API for frontend deployment

--- Frontend/src/utils/baseurl.js
+++ Frontend/src/utils/baseurl.js
@@ -1,2 +1,2 @@
-const baseUrl = "http://localhost:8000/api"
+const baseUrl = "https://aromatrace-ghkh.onrender.com/api";
 export default baseUrl;
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     open: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000/'
+        target: "https://aromatrace-ghkh.onrender.com"
       }
     }
   },
